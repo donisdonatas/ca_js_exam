e75bb3818f93f21ddb98ebbb25c52f2cb15cc6e0
donisdonatas <donisdonatas@gmail.com>
2022-11-07 19:53:22 +0200

    Trečios užduoties įkėlimas

--- JS Tasks 3/JS Tasks/Task 3/script.js
+++ JS Tasks 3/JS Tasks/Task 3/script.js
@@ -11,4 +11,50 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
+const showUsersBtn = document.getElementById('btn');
+showUsersBtn.addEventListener('click', fetchData);
+
 const ENDPOINT = 'https://api.github.com/users';
+
+const users = {};
+
+async function fetchData() {
+    try {
+        let response = await fetch(ENDPOINT);
+        if (response.ok) {
+            users.arr = await response.json();
+            showUsers(users.arr);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+function showUsers(users) {
+    //alert(users);
+    const msg = document.getElementById('message');
+    const output = document.getElementById('output');
+    output.removeChild(msg);
+    //messageClean.style.visibility = 'hidden';
+    //messageClean.remove();
+    users.forEach(user => {
+        const login = user.login;
+        const avatar = user.avatar_url;
+        //alert(login + avatar);
+        const card = document.createElement('div');
+        card.className = 'card';
+        const figure = document.createElement('figure');
+        const image = document.createElement('img');
+        image.setAttribute('src', avatar);
+        image.setAttribute('alt', `Avatar of user ${login}`);
+
+        const loginText = document.createElement('h3');
+        loginText.className = 'login';
+        loginText.textContent = login;
+
+        figure.append(image);
+        card.append(loginText, figure);
+        output.append(card);
+    });
+}
