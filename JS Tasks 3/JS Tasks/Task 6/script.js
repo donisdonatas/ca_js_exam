0b8bb9c8ca04ade9a2a41763788a1458fa8c37ee
donisdonatas <111775857+donisdonatas@users.noreply.github.com>
2022-11-14 12:32:00 +0200

    Revert "Revert "Dev""

--- JS Tasks 3/JS Tasks/Task 6/script.js
+++ JS Tasks 3/JS Tasks/Task 6/script.js
@@ -19,3 +19,9 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+const getUserAverageAge = users.reduce((sum, users) => sum + users.age, 0) / users.length;
+console.log(`Users average age is ${getUserAverageAge.toFixed(2)} years`);
+
+const getUsersNames = users.map(users => users.name);
+console.log(getUsersNames);
